1c8892b4252c2e2bcc009694831613a302bfc1a1
shin <shinsw627@naver.com>
2019-03-11 14:16:40 +0900

    aa

---
+++ sum/src/sum/sum.java
@@ -0,0 +1,16 @@
+package sum;
+
+public class sum {
+
+	public static void main(String[] args) {
+		int sum=0;
+		
+		for (int i=1;i<11;i++) {
+			sum = sum + i;
+			
+		}
+		System.out.print(sum);
+
+	}
+
+}
